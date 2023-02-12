5bbe62cf21801305617ef219cfb9a3b7c517c7b8
Garrett Gregor <garrett.gregor@gmail.com>
2023-02-12 09:16:13 -0700

    Initial commit

---
+++ friends.rb
@@ -0,0 +1,14 @@
+integers = [1, 4, 6, 66, 8, 25]
+
+# def numbers
+#     if integers >= 30
+#         puts true
+#     end
+# end
+
+integers.each do |integer|
+    if integer >= 30
+        p integer
+    end
+end
+
